50672fc274cb665fdbc4489dcda1176dd56f0feb
Matthew Castrillon-Madrigal <matthewcm64@gmail.com>
2022-05-30 22:15:09 +0100

    fullstack app complete

--- frontend/dist/frontend-be56642e14ccedd5.js
+++ frontend/dist/frontend-be56642e14ccedd5.js
@@ -269,7 +269,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('frontend-272c825b92ed12f3_bg.wasm', import.meta.url);
+        input = new URL('frontend-be56642e14ccedd5_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -659,12 +659,12 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper5806 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 326, __wbg_adapter_22);
+    imports.wbg.__wbindgen_closure_wrapper6051 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 338, __wbg_adapter_22);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper8730 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 410, __wbg_adapter_25);
+    imports.wbg.__wbindgen_closure_wrapper8975 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 422, __wbg_adapter_25);
         return addHeapObject(ret);
     };
 
