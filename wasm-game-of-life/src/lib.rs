3d0b92e6e1e6c783655a41df6fa68cabb67ba3ef
Matthew Castrillon-Madrigal <matthew.castrillon@accedo.tv>
2022-06-08 09:56:01 +0100

    random alive state

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -1,3 +1,4 @@
+extern crate js_sys;
 use wasm_bindgen::prelude::*;
 
 use std::fmt;
@@ -92,12 +93,13 @@ impl Universe {
         let height = 64;
 
         let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
+            .map(|_| {
+                if js_sys::Math::random() < 0.5 {
                     Cell::Alive
-                } else {
+                }else {
                     Cell::Dead
                 }
+
             })
             .collect();
 
